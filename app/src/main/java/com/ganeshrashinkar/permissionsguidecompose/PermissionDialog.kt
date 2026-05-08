a750633b7916e86820b6e97110e21d07105b44b8
Ganesh Rashinkr <ganeshrashinkar007@gmail.com>
2026-05-09 00:34:33 +0530

    Change font size in dialog description

--- app/src/main/java/com/ganeshrashinkar/permissionsguidecompose/PermissionDialog.kt
+++ app/src/main/java/com/ganeshrashinkar/permissionsguidecompose/PermissionDialog.kt
@@ -5,8 +5,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Divider
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -54,7 +54,9 @@ fun PermissionDialog(
             }
         },
         title = {
-            Text(permissionTextProvider.getDescription(isPermissionDeclined))
+            Text(permissionTextProvider.getDescription(isPermissionDeclined),
+                style = MaterialTheme.typography.bodyMedium
+                )
         },
         modifier = modifier
     )
